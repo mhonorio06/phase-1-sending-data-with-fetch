8c94d6f86df756e48b069e714d56b28d833969c7
Manny <mhfernan35277@gmail.com>
2023-08-08 22:01:30 -0700

    complete POST lab

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 // Add your code here
-function submitData( name, email ) {
-    return fetch( 'http://localhost:3000/users', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify( {
-            name,
-            email
+function submitData (name, email) {
+
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
         })
-      })
-      .then( function ( response ) {
-        return response.json()
-      } )
-      .then( function ( data ) {
-        document.body.innerHTML = data[ "id" ]
-      } )
-      .catch( function ( error ) {
-        document.body.innerHTML = error.message        
-      } )
-  }
-
-
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        document.body.textContent = data.id
+    })
+    .catch(function (error) {
+        document.body.textContent = error.message;
+    })
+}
 
